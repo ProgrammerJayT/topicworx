7946cefb2faca958d248411b8b249a7712b3e979
Johannes Tiyasi <theanthem8@gmail.com>
2025-03-09 12:48:07 +0200

    Create table visualizer

---
+++ src/app/components/table/index.jsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TablePagination from "@mui/material/TablePagination";
+
+const CovidTable = ({ data = [] }) => {
+  if (data.length === 0) return null;
+
+  const headers = Object.keys(data[0]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="covid data table">
+        <TableHead>
+          <TableRow>
+            {headers.map((header) => (
+              <TableCell
+                key={header}
+                align={header === "Date" ? "left" : "right"}
+              >
+                {header}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((row, index) => (
+              <TableRow key={index}>
+                {headers.map((header) => (
+                  <TableCell
+                    key={header}
+                    align={header === "Date" ? "left" : "right"}
+                  >
+                    {row[header]}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </TableContainer>
+  );
+};
+
+export default CovidTable;
